81fb68f200481334ec8d61e2e51cc0d34c2e36ce
Atul Singh <atul.ks1998@gmail.com>
2021-07-20 13:36:30 +0530

    navbar added

--- src/Nav.js
+++ src/Nav.js
@@ -1,11 +1,29 @@
-import React from 'react'
-
+import React ,{useState,useEffect} from 'react';
+import './Nav.css';
 function Nav() {
+    const[show,setShow] =useState(false);
+    useEffect(() => {
+        window.addEventListener("scroll",()=>{
+            if(window.scrollY>100){
+                setShow(true);
+            }
+            else{
+                setShow(false);
+            }
+        });
+        return ()=>{
+            window.removeEventListener("scroll");
+        };
+    },[]);
     return (
-        <div>
-            
+        <div className={`nav ${show &&  "nav_black"}`}>
+            <img
+            className='Nav__logo'
+             src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
+            alt="Logo"
+             />
         </div>
-    )
-}
+        );
+    }
 
-export default Nav
+export default Nav;
